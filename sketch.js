3fa4b7a5acb16169ae914c6fafbdd8cd75dfa0c5
Zyquan Thomas <zthomas@wlapcs.org>
2017-04-11 17:10:11 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var x
+var y 
+
+x = 200
+y = 100 
+
+
+function setup(){
+    createCanvas(600,400)
+}
+function draw(){
+    background(54);
+    fill(242, 7, 7);
+    ellipse(500 ,y,100,100);
+    fill(105,39,100);
+    rect(150,y,200,250);
+    
+    
+}
