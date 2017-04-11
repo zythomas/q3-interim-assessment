4350167c4c33869e67bc9ee822e02d99f8c6b320
Zyquan Thomas <zthomas@wlapcs.org>
2017-04-11 18:39:47 +0000

    first commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
-var x
+
+
 var y 
 
-x = 200
 y = 100 
 
 
@@ -11,9 +11,24 @@ function setup(){
 function draw(){
     background(54);
     fill(242, 7, 7);
-    ellipse(500 ,y,100,100);
+    var x = 0;
+    ellipse(x ,y,100,100);
+    x = x+50;
+    ellipse(x ,y,100,100);
+     x = x+50;
+    ellipse(x ,y,100,100);
+     x = x+50;
+    ellipse(x ,y,100,100);
+     x = x+50;
     fill(105,39,100);
-    rect(150,y,200,250);
+    rect(150,10,400,30);
     
-    
+    if(mouseIsPressed){
+        background(250,0,50);
+    }
+    var size = [10, 20, 40, 80, 160];
+    for(var i = 0; i < size.length ; i++){
+        ellipse((i+1)*100, 300, size[i], size [i]);
+    }
 }
+
